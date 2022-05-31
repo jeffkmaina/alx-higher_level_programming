cd3debb585dde66aa53322bf71531f8d938f8561
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-05-31 21:01:30 +0000

    task 1 correction of pep

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,9 @@ elif number < 0:
     num = number % (-10)
 
 if num > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, num))
-elif num == 0:
+    print("Last digit of {} is {} and is\
+            greater than 5".format(number, num))
+elif num is 0:
     print("Last digit of {} is {} and is 0".format(number, num))
 if num != 0 and num < 6:
     print("Last digit of {} is {} and is less\
