de751cf9e78f50810146bca6e3b6a84ba1924a42
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-06-21 18:30:08 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+class Square
+"""
+
+
+class Square:
+    """ defines a square"""
+    def __init__(self, size):
+        """
+        creates instance of a sqaure
+
+        Args:
+        size: Size of the square
+        """
+        self.__size = size
